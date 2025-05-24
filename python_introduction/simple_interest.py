bd759ef7140a97eb88eccc4298b495cbd37144ec
adynah <t.barimalaa@gmail.com>
2025-05-24 03:28:01 +0100

    Add simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Define the variables
+principal = 1000 
+rate = 5
+time = 3 
+
+#Calculate th interest(I)
+I = P * R * T
+interest = principal * rate * time
+
+# Print the result
+print(f"The simple interest is: {interest}")
