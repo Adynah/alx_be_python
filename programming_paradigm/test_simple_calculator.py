d1bbd3623a50f5d83456a268efedc1f980d1bb21
adynah <t.barimalaa@gmail.com>
2025-06-18 23:01:05 +0100

    Ran Unit Test for a Simple Calculator Class - corrected operator spelling

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,25 +14,25 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 5), 4)
 
     # Test for substraction
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual (self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-5, -2), -3)
     
     # Test for multiplication
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual (self.calc.multiply(0, 10), 0)
         self.assertEqual (self.calc.multiply(4, -3), -12)
         self.assertEqual (self.calc.multiply(-5, -2), 10)
     
     # Test for division
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual (self.calc.divide(12, 2), 6)
         self.assertEqual (self.calc.divide(8, 8), 1)
         self.assertEqual (self.calc.divide(0, 5), 0)
     
     # Test dividion by zero
-    def test_divide_by_zero(self):
+    def test_division_by_zero(self):
         self.assertEqual(self.calc.divide(4, 0), None)
 
 
