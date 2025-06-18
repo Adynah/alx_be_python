4c90526dfe3e6173507326c957b263d9c08efb8a
adynah <t.barimalaa@gmail.com>
2025-06-18 23:03:50 +0100

    Ran Unit Test for a Simple Calculator Class - corrected inline spacing

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,27 +9,27 @@ class TestSimpleCalculator(unittest.TestCase):
     # Test for addition
     def test_addition(self):
         """Test the addition method"""
-        self.assertEqual (self.calc.add (2, 3), 5)
-        self.assertEqual (self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add (2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-1, 5), 4)
 
     # Test for substraction
     def test_subtraction(self):
-        self.assertEqual (self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-5, -2), -3)
     
     # Test for multiplication
     def test_multiplication(self):
-        self.assertEqual (self.calc.multiply(0, 10), 0)
-        self.assertEqual (self.calc.multiply(4, -3), -12)
-        self.assertEqual (self.calc.multiply(-5, -2), 10)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(4, -3), -12)
+        self.assertEqual(self.calc.multiply(-5, -2), 10)
     
     # Test for division
     def test_division(self):
-        self.assertEqual (self.calc.divide(12, 2), 6)
-        self.assertEqual (self.calc.divide(8, 8), 1)
-        self.assertEqual (self.calc.divide(0, 5), 0)
+        self.assertEqual(self.calc.divide(12, 2), 6)
+        self.assertEqual(self.calc.divide(8, 8), 1)
+        self.assertEqual(self.calc.divide(0, 5), 0)
     
     # Test dividion by zero
     def test_division_by_zero(self):
